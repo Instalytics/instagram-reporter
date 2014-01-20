1de1c91aa152e523b5a3547e1484af27e856e49e
mpikula <mariusz.pikula@gmail.com>
2014-01-20 16:04:44 +0100

    database connection initialization

--- lib/instagram/reporter/instagram_reporter.rb
+++ lib/instagram/reporter/instagram_reporter.rb
@@ -17,6 +17,17 @@ require 'instagram_popular_accounts_finder'
 
 module Instagram
   class InstagramReporter
+    
+    def intialize
+      if ENV['MONGOHQ_URL'].empty?
+        raise "Please configure database connection URI under MONGOHQ_URL environmental variable"
+      else
+        #check if environmental variables are set for mongoid 
+        connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
+        MongoConfig.connection = connection
+      end
+    end
+
     def get_all_popular_instagram_accounts
       instagram_accounts = InstagramPopularAccountsFinder.new.get_instagram_accounts
       #puts
