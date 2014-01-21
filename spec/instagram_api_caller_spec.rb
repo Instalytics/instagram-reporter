9ea4179a67b1e7d483d90f25d6237719b2599997
luki3k5 <luki3k5@gmail.com>
2014-01-21 10:46:05 +0100

    more proper testing

--- spec/instagram_api_caller_spec.rb
+++ spec/instagram_api_caller_spec.rb
@@ -17,6 +17,12 @@ describe InstagramApiCaller do
         expect(subject.get_instagram_accounts.class).to eq(Array)
       end
     end
+
+    it 'returns parsed data' do
+      VCR.use_cassette('get_instagram_accounts') do
+        expect(subject.get_instagram_accounts.size).to eq(16)
+      end
+    end
   end
 
   describe '#get_hashtag_info' do
@@ -30,7 +36,7 @@ describe InstagramApiCaller do
     it 'returns 20 media files infos inside' do
       VCR.use_cassette('get_hashtag_info') do
         response = subject.get_hashtag_info(test_hashtag)
-        expect(response.size).to  eq(20)
+        expect(response.size).to eq(20)
       end
     end
 
