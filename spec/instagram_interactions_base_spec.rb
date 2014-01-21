daa0974b46bd19e69e3c01bc49a1f4d35bf89a7e
mpikula <mariusz.pikula@gmail.com>
2014-01-21 13:01:52 +0100

    added additional rspec

--- spec/instagram_interactions_base_spec.rb
+++ spec/instagram_interactions_base_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe InstagramInteractionsBase do
 
   describe '#initialize' do
-    xit "should raise error if environmental variable INSTAGRAM_API_TOKEN is not set" do
+    it "should raise error if environmental variable INSTAGRAM_API_TOKEN is not set" do
       InstagramInteractionsBase::API_TOKEN = nil
-      expect(subject).to raise_error
+      expect{InstagramInteractionsBase.new}.to raise_error(RuntimeError, 'INSTAGRAM_API_TOKEN environment variable not set')
     end
     it "should assign envirnomental variable to appropriate class variable if env variable is not empty" do
       InstagramInteractionsBase::API_TOKEN ="SAMPLE_API_TOKEN"
