484986fae149184b86b1374d8e3987e5ea5affbe
luki3k5 <luki3k5@gmail.com>
2014-01-21 10:54:11 +0100

    experimenting with chaining exceptions up the stack

--- lib/instagram_reporter/instagram_api_caller.rb
+++ lib/instagram_reporter/instagram_api_caller.rb
@@ -13,26 +13,34 @@ class InstagramApiCaller < InstagramInteractionsBase
   end
 
   def get_instagram_accounts
-    response = @api_connection.get do |req|
-      req.url "#{POPULAR_INSTAGRAM_MEDIA_URL}?client_id=#{API_TOKEN}"
-      req.options = DEFAULT_REQUEST_OPTIONS
-    end
+    begin
+      response = @api_connection.get do |req|
+        req.url "#{POPULAR_INSTAGRAM_MEDIA_URL}?client_id=#{API_TOKEN}"
+        req.options = DEFAULT_REQUEST_OPTIONS
+      end
 
-    parse_json(response.body)
+      return parse_json(response.body)
+    rescue Exception => ex
+      raise ex
+    end
   end
 
   def get_hashtag_info(tag)
-    response = @api_connection.get do |req|
-      req.url "/v1/tags/#{tag}/media/recent?client_id=#{API_TOKEN}"
-      req.options = DEFAULT_REQUEST_OPTIONS
-    end
+    begin
+      response = @api_connection.get do |req|
+        req.url "/v1/tags/#{tag}/media/recent?client_id=#{API_TOKEN}"
+        req.options = DEFAULT_REQUEST_OPTIONS
+      end
 
-    parse_json(response.body)
+      return parse_json(response.body)
+    rescue Exception => ex
+      raise ex
+    end
   end
 
 
   private
-    def parse_json(data)
-      Oj.load(data)['data']
-    end
+  def parse_json(data)
+    Oj.load(data)['data']
+  end
 end
