46076b72c585c6a1639c46c8039b0e399f644241
Maciej Ksiazek <xsms@poczta.onet.pl>
2014-04-14 15:54:09 +0200

    InstagramApiCaller rescue on api calls moved to a single place

--- lib/instagram_reporter/instagram_api_caller.rb
+++ lib/instagram_reporter/instagram_api_caller.rb
@@ -13,26 +13,18 @@ class InstagramApiCaller < InstagramInteractionsBase
 
   def get_instagram_accounts_by_api_token
     instagram_api_get_and_parse(POPULAR_INSTAGRAM_MEDIA_URL)
-  rescue Exception => ex
-    raise ex, "Failed to obtain instagram accounts by api token wit exception #{ex.inspect} for response #{response.inspect}"
   end
 
   def get_instagram_accounts_by_access_token(access_token) 
     instagram_api_get_and_parse(POPULAR_INSTAGRAM_MEDIA_URL, access_token)
-  rescue Exception => ex
-    raise ex, "Failed to obtain instagram accounts by access token with exception #{ex.inspect} for response #{response.inspect}"
   end
 
   def get_hashtag_info_by_access_token(tag, access_token)
     instagram_api_get_and_parse("/v1/tags/#{tag}/media/recent", access_token)
-  rescue Exception => ex
-    raise "Failed to obtain hashtag info by access token for tag #{tag} with exception #{ex.inspect} for response #{response.inspect} "
   end
 
   def get_hashtag_info_by_api_token(tag)
     instagram_api_get_and_parse("/v1/tags/#{tag}/media/recent")
-  rescue Exception => ex
-    raise "Failed to obtain hashtag info by api token for tag #{tag} with exception #{ex.inspect} for response #{response.inspect}"
   end
 
   def call_api_by_access_token_for_media_file_comments(instagram_media_id,access_token)
@@ -63,7 +55,22 @@ class InstagramApiCaller < InstagramInteractionsBase
         req.url "#{url}?#{query_params(access_token)}"
         req.options = DEFAULT_REQUEST_OPTIONS
       end
-      parse_json(response.body)
+      parse_response(response)
+    end
+
+    def parse_response(response)
+      case response.status
+      when 200
+        parse_json(response.body)
+      when 404, 500, 502, 503, 504
+        {
+          result: 'error',
+          body: response.body,
+          status: response.status
+        }
+      end
+    rescue Exception => ex
+      raise ex, "Failed to obtain instagram data for url #{url} #{ex.inspect}. response body is : #{response.inspect}"
     end
 
     def query_params(access_token)
