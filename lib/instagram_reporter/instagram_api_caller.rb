a304d36546126095bbe9b769f4ce875ef147960a
Maciej Ksiazek <xsms@poczta.onet.pl>
2014-04-30 12:01:03 +0200

    cleaned up InstagramApiCaller constructor from setting @api_connection.  Extracted it to separate method and removed direct variable usage.

--- lib/instagram_reporter/instagram_api_caller.rb
+++ lib/instagram_reporter/instagram_api_caller.rb
@@ -2,15 +2,6 @@
 
 class InstagramApiCaller < InstagramInteractionsBase
 
-  def initialize
-    super
-    @api_connection = Faraday.new(url: API_BASE_URL) do |faraday|
-      faraday.request  :url_encoded
-      faraday.use FaradayMiddleware::FollowRedirects
-      faraday.adapter  Faraday.default_adapter
-    end
-  end
-
   def get_instagram_accounts_by_api_token
     uri = "#{API_BASE_URL}#{POPULAR_INSTAGRAM_MEDIA_URL}?#{query_params(nil)}"
     instagram_api_get_and_parse(uri)
@@ -55,7 +46,6 @@ class InstagramApiCaller < InstagramInteractionsBase
   end
 
   private
-    attr_reader :api_connection
 
     def parse_json(data)
       Oj.load(data)['data']
@@ -107,7 +97,7 @@ class InstagramApiCaller < InstagramInteractionsBase
     end
 
     def call_api_by_access_token_for_media_info(instagram_media_id, access_token , action)
-      response = @api_connection.get do |req|
+      response = api_connection.get do |req|
         req.url "/v1/media/#{instagram_media_id}?access_token=#{access_token}"
         req.options = DEFAULT_REQUEST_OPTIONS
       end
@@ -122,7 +112,7 @@ class InstagramApiCaller < InstagramInteractionsBase
     end
 
     def call_api_by_api_token_for_media_file(media_id, action)
-      response = @api_connection.get do |req|
+      response = api_connection.get do |req|
         req.url "/v1/media/#{media_id}?client_id=#{API_TOKEN}"
         req.options = DEFAULT_REQUEST_OPTIONS
       end
@@ -133,6 +123,14 @@ class InstagramApiCaller < InstagramInteractionsBase
         return {result: 'error', body: Oj.load(response.body)}
       else
         raise "call for media #{action} (media_id: #{media_id}) failed with response #{response.inspect}"
+      end
+    end
+
+    def api_connection
+      @api_connection ||= Faraday.new(url: API_BASE_URL) do |faraday|
+        faraday.request  :url_encoded
+        faraday.use FaradayMiddleware::FollowRedirects
+        faraday.adapter  Faraday.default_adapter
       end
     end
 end
